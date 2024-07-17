0e45e604f4b3a3b25737390c0ad74fdc8ad652a9
unknown <albert.jordaan@outlook.com>
2024-07-17 17:02:40 +1200

    Pushing Latest tailwind and other pages, still working on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ["C:/Users/Albert/Documents/Albert's Files/LoginTemplates/src/register.html"],
+  content: [
+    "C:/Users/Albert/Documents/Albert's Files/LoginTemplates/src/register.html",
+    "C:/Users/Albert/Documents/Albert's Files/LoginTemplates/src/registertailwind.html",
+    "C:/Users/Albert/Documents/Albert's Files/LoginTemplates/src/checkout.html",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
